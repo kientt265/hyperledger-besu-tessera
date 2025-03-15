be7d38fe5729b9110db180200dc652833e68edaa
kientt265 <kientran26503@gmail.com>
2025-03-15 14:01:06 +0700

    add-config-tessera

---
+++ node6/Tessera/startTessera.js
@@ -0,0 +1,38 @@
+const { exec } = require('child_process');
+const path = require('path');
+
+const startTessera = () => {
+    const currentDir = __dirname;
+    
+    const dockerCommand = `docker run -v ${currentDir}:/app -p 9106:9106 --name tessera-node6 --rm quorumengineering/tessera:latest -configfile /app/config.json`;
+
+    console.log('Starting Tessera node...');
+    console.log(`Working directory: ${currentDir}`);
+
+    const tessera = exec(dockerCommand);
+
+    tessera.stdout.on('data', (data) => {
+        console.log(`Tessera: ${data}`);
+    });
+
+    tessera.stderr.on('data', (data) => {
+        console.error(`Tessera Error: ${data}`);
+    });
+
+    tessera.on('close', (code) => {
+        console.log(`Tessera process exited with code ${code}`);
+    });
+
+    // Handle process termination
+    process.on('SIGINT', () => {
+        console.log('Stopping Tessera node...');
+        exec('docker stop tessera-node6', (error) => {
+            if (error) {
+                console.error('Error stopping container:', error);
+            }
+            process.exit();
+        });
+    });
+}
+
+startTessera();
